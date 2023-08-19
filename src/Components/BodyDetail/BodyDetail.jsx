610da88d8ba37f91e901ea4a4dc799c145e08f0f
Meezan-cool <nmeezan4@gmail.com>
2023-08-20 01:44:07 +0530

    line-trough

--- src/Components/BodyDetail/BodyDetail.jsx
+++ src/Components/BodyDetail/BodyDetail.jsx
@@ -1,30 +1,55 @@
-import React from 'react'
-import "./BodyDetail.css"
-import Blur from '../../Imageslogo/blur 1.svg'
-import Delete from '../../Imageslogo/delete 1.svg'
-import Check from '../../Imageslogo/checked 1.svg'
-const BodyDetail = ({data}) => {
+import React, { useState } from 'react';
+import "./BodyDetail.css";
+import Blur from '../../Imageslogo/blur 1.svg';
+import Delete from '../../Imageslogo/delete 1.svg';
+import Check from '../../Imageslogo/checked 1.svg';
+
+const BodyDetail = ({ data }) => {
+  const [selectedTasks, setSelectedTasks] = useState([]);
+
+  const handleTaskClick = (id) => {
+    if (selectedTasks.includes(id)) {
+      setSelectedTasks(selectedTasks.filter(taskId => taskId !== id));
+    } else {
+      setSelectedTasks([...selectedTasks, id]);
+    }
+  };
+
   return (
     <div className='Body_detail'>
-    <div className='sub_Body'>
-      {data.map((item,id)=>{
-      return(
-           <div key={id} className='map_cont'>
-            <div><img src={Blur} alt="" /></div>
-           <div className='content'>
-           {item.task.slice(0,60)}...
-           </div>
-           <div className='action_buttons'>
-           <div><img src={Check} alt="" /></div>
-           <div><img src={Delete} alt="" /></div>
-           </div>
-           </div>
-      )
-      })
-      }
+      <div className='sub_Body'>
+        {data.map((item, id) => {
+          const isSelected = selectedTasks.includes(id);
+          return (
+            <div key={id} className={`map_cont ${isSelected ? 'selected' : ''}`}>
+              <div><img src={Blur} alt="" /></div>
+              <div className='content'>
+                {item.task.slice(0, 60)}...
+              </div>
+              <div className='action_buttons'>
+                <div onClick={() => handleTaskClick(id)}>
+                  <img src={Check} alt="" />
+                </div>
+                <div><img src={Delete} alt="" /></div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
-export default BodyDetail
+export default BodyDetail;
+
+// const [expandedTasks, setExpandedTasks] = useState([])
+// const handleCheckClick = (id) => {
+//   if (expandedTasks.includes(id)) {
+//     setExpandedTasks(expandedTasks.filter(item => item !== id));
+//   } else {
+//     setExpandedTasks([...expandedTasks, id]);
+//   }
+// };
+//<div className='content'>
+//{expandedTasks.includes(id) ? item.task : item.task.slice(0, 60) + '...'}
+//</div> 
